eb355a7c250322aba13b0de849c6bcf278812585
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2022-11-27 22:34:19 +0100

    docs: updated readme; chore: removed old startup command from docker compose file

--- app/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Startup expressjs & socket.io backend using nodemon worker
-(cd backend; npm run build; npm run start)
-
-# Startup reactjs frontend using nodemon worker
-(cd frontend; npm run build)
