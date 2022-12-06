7bac154a717bc08ec45ae58968fe26fdc3d4e274
Tobias Franz <cinex.mail@web.de>
2022-12-06 21:41:34 +0100

    feature: member list for the chat app
    Signed-off-by: Tobias Franz <cinex.mail@web.de>

--- app/frontend/src/framework/screens/GameRoom/GameRoom.tsx
+++ app/frontend/src/framework/screens/GameRoom/GameRoom.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import RoomManager from "../../util/RoomManager";
 import ModuleRegistry from "../../modules/ModuleRegistry";
+import User from "../../util/User";
+import ProfileImage from "../../components/ProfileImage/ProfileImage";
+import ProfileManager from "../../util/ProfileManager";
 
 export default class GameRoom extends React.Component {
+
+    renderMember(member: User) {
+        return (
+            <div className="member-list-row" key={member.getId()}>
+                <ProfileImage picture={member.getPicture()}
+                              username={member.getUsername()}
+                              id={member.getId()} />
+                {member.getUsername()}
+                {(member.getId() === ProfileManager.getId() ? <span>&nbsp;(You)</span> : null)}
+                {(member.isRoomMaster() ? <span className="signature-text">&nbsp;A</span> : null)}
+            </div>
+        );
+    }
 
     render() {
         let currentGameId = RoomManager.getCurrentGameId();
@@ -18,6 +34,7 @@ export default class GameRoom extends React.Component {
 
         return (
             <div id="screenGame">
+                <div id="memberList">{RoomManager.getRoomMembers().map(this.renderMember)}</div>
                 {currentGameModule.renderGame()}
             </div>
         );
